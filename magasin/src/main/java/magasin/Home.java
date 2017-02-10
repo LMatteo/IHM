b00a2012ecf4636b22c91a79ee50a460d3a2fc5c
Miikzy <no.rules@live.fr>
2017-02-10 21:28:36 +0100

    added method to change scene in client homepage boutique

--- magasin/src/main/java/magasin/Home.java
+++ magasin/src/main/java/magasin/Home.java
@@ -1,7 +1,15 @@
 package magasin;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * @author Zaki
@@ -15,8 +23,7 @@ public class Home{
     }
 
     @FXML
-    void goToBook(MouseEvent event) {
-
+    void goToBook(MouseEvent event) throws IOException{
     }
 
     @FXML
@@ -30,8 +37,7 @@ public class Home{
     }
 
     @FXML
-    void goToCatalog(MouseEvent event) {
-
+    void goToCatalog(MouseEvent event) throws IOException{
     }
 
     @FXML
@@ -48,6 +54,16 @@ public class Home{
     void goToStage(MouseEvent event) {
 
     }
+
+    private void switchScene(MouseEvent event, String location) throws IOException {
+        Node node = (Node) event.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        Parent root = FXMLLoader.load(getClass().getResource(location));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
 
 }
 
