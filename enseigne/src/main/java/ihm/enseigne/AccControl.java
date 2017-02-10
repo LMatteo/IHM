443e8e38ba02b67d590c8679a1aa1afcf5498d18
Matteo Lucas <lucas.matteo@etu.unice.fr>
2017-02-10 18:09:49 +0100

    add pom

--- enseigne/src/main/java/ihm/enseigne/AccControl.java
+++ enseigne/src/main/java/ihm/enseigne/AccControl.java
@@ -1,10 +1,18 @@
-package sample;
+package ihm.enseigne;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 public class AccControl {
+
+    @FXML
+    private ScrollPane pane;
 
     @FXML
     private Button accueil;
@@ -19,8 +27,9 @@ public class AccControl {
     private Button info;
 
     @FXML
-    void goToAcc(ActionEvent event) {
-
+    void goToAcc(ActionEvent event) throws IOException{
+        VBox box = FXMLLoader.load(getClass().getResource("../resources/home.fxml"));
+        pane.setContent(box);
     }
 
     @FXML
