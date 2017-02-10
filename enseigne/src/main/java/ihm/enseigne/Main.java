94de73ce070992bc14a933de20a0a0062603c8d3
Matteo Lucas <lucas.matteo@etu.unice.fr>
2017-02-10 18:17:12 +0100

    working

--- enseigne/src/main/java/ihm/enseigne/Main.java
+++ enseigne/src/main/java/ihm/enseigne/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader();
-        Parent root = loader.load(getClass().getResource("skel.fxml"));
+        Parent root = loader.load(getClass().getResource("/skel.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 1280, 1024));
         primaryStage.show();
