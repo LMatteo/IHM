c8b0481b5a9a6335554acf85148374e81f31d4ab
Miikzy <no.rules@live.fr>
2017-02-10 21:03:06 +0100

    added the client homepage boutique

---
+++ magasin/src/main/java/magasin/Home.java
@@ -0,0 +1,53 @@
+package magasin;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * @author Zaki
+ */
+
+public class Home{
+
+    @FXML
+    void goToActu(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToBook(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToBoutique(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToCD(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToCatalog(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToMall(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToPromo(MouseEvent event) {
+
+    }
+
+    @FXML
+    void goToStage(MouseEvent event) {
+
+    }
+
+}
+
