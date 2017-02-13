f022a4fd2d73fa6810b7aa46aac02f333ddf76f8
josue.c <jcr.clb@gmail.com>
2017-02-13 13:31:58 +0100

    added "salut"

--- enseigne/src/main/java/ihm/enseigne/Main.java
+++ enseigne/src/main/java/ihm/enseigne/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader();
         Parent root = loader.load(getClass().getResource("/skel.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("salut");
         primaryStage.setScene(new Scene(root, 1280, 1024));
         primaryStage.show();
     }
