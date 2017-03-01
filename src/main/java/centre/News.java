b5512b5932ad8207aa4b623ec6e78302a4159b15
JA Puskaric <jean-adam.puskaric@etu.unice.fr>
2017-03-01 19:55:31 +0100

    Push de la muerte avec le dbéut de l'interface admin pour le style des actualités - création de données non fonctionnel atm, choix possibles mais ca met pas encore la case.

---
+++ src/main/java/centre/News.java
@@ -0,0 +1,61 @@
+package centre;
+
+
+import java.io.File;
+import java.io.IOException;
+
+public class News {
+
+    private long date;
+    private int position;
+    private String french;
+    private String english;
+    private String magasinId;
+    private boolean horizontal;
+    private String name;
+
+    /**
+     * Creates a news object with the data provided
+     *
+     * @param file - the file containing the data
+     * @throws IOException - if failing to read the file
+     */
+    public News(File file) throws IOException {
+        NewsParser np = new NewsParser(file);
+        date = np.getDate();
+        position = np.getPosition();
+        french = np.getProperty("french");
+        english = np.getProperty("english");
+        magasinId = np.getProperty("magasinId");
+        name = np.getProperty("name");
+        horizontal = np.horizontal();
+    }
+
+    public boolean isUsed() {
+        return (position != 0);
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public String getFrench() {
+        return french;
+    }
+
+    public String getEnglish() {
+        return english;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public boolean isHorizontal() {
+        return horizontal;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/centre/NewsParser.java
@@ -0,0 +1,64 @@
+package centre;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+/**
+ * A parser used to read the JSON files used to store store data.
+ */
+public class NewsParser {
+
+    private JSONObject json;
+
+    /**
+     * Initializes this news parser. Reads the content of the json file,
+     * and converts it to a JsonObject.
+     *
+     * @param input - the json file containing the store data
+     * @throws IOException - if failing to read the file
+     */
+    public NewsParser(File input) throws IOException {
+        json = new JSONObject(new String(Files.readAllBytes(input.toPath())));
+    }
+
+    /**
+     * Returns the data associated to the requested key.
+     *
+     * @param key - the key of the store data to retrieve
+     * @return the data corresponding to this key
+     */
+    public String getProperty(String key) {
+        return json.getString(key);
+    }
+
+    /**
+     * Returns the position of the news in the feed or 0.
+     *
+     * @return the position of this news
+     */
+    public int getPosition() {
+        return json.getInt("position");
+    }
+
+    /**
+     * Returns the position of the news in the feed or 0.
+     *
+     * @return the position of this news
+     */
+    public long getDate() {
+        return json.getLong("date");
+    }
+
+    /**
+     * Returns the position of the news in the feed or 0.
+     *
+     * @return the position of this news
+     */
+    public boolean horizontal() {
+        return json.getBoolean("horizontal");
+    }
+
+}
