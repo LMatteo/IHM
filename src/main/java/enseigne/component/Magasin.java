c8c96379c9b6f946d201d7d16dc80e611364d45d
Lucas Matteo <lucas.matteo@etu.unice.fr>
2017-03-01 16:53:13 +0100

    creating enseigne

---
+++ src/main/java/enseigne/component/Magasin.java
@@ -0,0 +1,14 @@
+package enseigne.component;
+
+public class Magasin {
+    private String name;
+    private String photo;
+    private String addr;
+    private String web;
+    private String centre;
+    private String info;
+
+    private int affaire;
+    private int rendu;
+    private int nbEmpl;
+}
