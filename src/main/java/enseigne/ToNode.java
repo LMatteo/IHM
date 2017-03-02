d8614d721b20dc01ea73401aca90f1dc5ca41403
Lucas Matteo <lucas.matteo@etu.unice.fr>
2017-03-02 14:46:38 +0100

    adding mag

---
+++ src/main/java/enseigne/ToNode.java
@@ -0,0 +1,21 @@
+package enseigne;
+
+import enseigne.component.Photo;
+import enseigne.component.actu.Actu;
+import enseigne.component.magasin.Magasin;
+import javafx.scene.Node;
+
+public class ToNode {
+
+    public static Node magasins(Magasin m){
+        return null;
+    }
+
+    public static Node actu(Actu actu){
+        return null;
+    }
+
+    public static Node photos(Photo p){
+        return null;
+    }
+}
