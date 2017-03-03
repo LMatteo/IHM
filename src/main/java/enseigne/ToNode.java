0b0614e442e034bd007b486049ed0ac1201dbdae
josue.c <jcr.clb@gmail.com>
2017-03-03 15:39:59 +0100

    updated tonode store

--- src/main/java/enseigne/ToNode.java
+++ src/main/java/enseigne/ToNode.java
@@ -11,27 +11,34 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.File;
+import java.util.Objects;
 
 public class ToNode {
 
-    public static Node magasins(Magasin m){
+    public static Node magasins(Magasin m) {
         HBox hbox1 = new HBox();
         ImageView p = new ImageView();
 
-        if(m.getPhoto() != null) {
+        if (m.getPhoto() != null) {
             p.setImage(new Image(new File(m.getPhoto()).toURI().toString()));
             p.setFitWidth(200);
             p.setFitHeight(200);
             p.setStyle("-fx-padding: 20px");
         }
         VBox vbox2 = new VBox();
-        Label label1 = new Label(m.getVille()+", au centre commercial "+m.getCentre());
+        Label label1 = new Label();
+        System.out.println(m.getVille());
+        if (Objects.equals(m.getVille(), null)) {
+            label1.setText("Au centre commercial " + m.getCentre());
+        } else {
+            label1.setText("À "+m.getVille() + ", au centre commercial " + m.getCentre());
+        }
+
         Label label2 = new Label(m.getInfoFr());
-        Label label3 = new Label("Téléphone : "+m.getTelephone());
-        Label label4 = new Label("Site web : "+m.getWeb());
-        Label label5 = new Label("Adresse : "+m.getAddr()+"\n"+m.getCodePostal()+" "+m.getVille());
+        Label label3 = new Label("Téléphone : " + m.getTelephone()+" - Site web : " + m.getWeb());
+        Label label5 = new Label("Adresse : " + m.getAddr() + " " + m.getCodePostal() + " " + m.getVille());
         HBox hbox3 = new HBox();
-        hbox3.getChildren().addAll(label3,label4);
+        hbox3.getChildren().add(label3);
 
         vbox2.getChildren().add(label1);
         vbox2.getChildren().add(label2);
@@ -48,43 +55,43 @@ public class ToNode {
         return hbox1;
     }
 
-    public static Node actu(Actu a){
+    public static Node actu(Actu a) {
         HBox hbox1 = new HBox();
         ImageView img = new ImageView();
-        if(a.getImage() != null) {
+        if (a.getImage() != null) {
             img.setImage(new Image(new File(a.getImage()).toURI().toString()));
             img.setFitWidth(200);
             img.setFitHeight(200);
         }
-        if(a.isBigSize()){
+        if (a.isBigSize()) {
             hbox1.getChildren().add(img);
             VBox vbox2 = new VBox();
             Label l1 = new Label(a.getTitreFr());
             Label l2 = new Label(a.getContentFr());
-            vbox2.getChildren().addAll(l1,l2);
+            vbox2.getChildren().addAll(l1, l2);
             hbox1.getChildren().add(vbox2);
 
         } else {
             VBox vbox2 = new VBox();
             vbox2.getChildren().add(img);
-            vbox2.getChildren().addAll(new Label(a.getTitreFr()),new Label(a.getContentFr()));
+            vbox2.getChildren().addAll(new Label(a.getTitreFr()), new Label(a.getContentFr()));
         }
-    return hbox1;
+        return hbox1;
     }
 
-    public static Node photos(Photo p){
+    public static Node photos(Photo p) {
         HBox hbox1 = new HBox();
         VBox vBox2 = new VBox();
         ImageView img = new ImageView();
-        if(p.getPhoto() != null) {
+        if (p.getPhoto() != null) {
             img.setImage(new Image(new File(p.getPhoto()).toURI().toString()));
             img.setFitWidth(200);
             img.setFitHeight(200);
         }
-        vBox2.getChildren().addAll(new Label(p.getTitreFr()),img);
-        Label l = new Label("Catégorie : "+p.getCategory());
-        Label ll = new Label("Description : "+p.getDescriptionFr());
-        vBox2.getChildren().addAll(ll,l);
+        vBox2.getChildren().addAll(new Label(p.getTitreFr()), img);
+        Label l = new Label("Catégorie : " + p.getCategory());
+        Label ll = new Label("Description : " + p.getDescriptionFr());
+        vBox2.getChildren().addAll(ll, l);
         return hbox1;
     }
 }
