205e5edb74b03f2e2105c16160a54f773acccd02
josue.c <jcr.clb@gmail.com>
2017-03-03 16:26:32 +0100

    updated pleins de trucs mais qui servent a rien au final

--- src/main/java/enseigne/component/ReadConst.java
+++ src/main/java/enseigne/component/ReadConst.java
@@ -1,12 +1,6 @@
 package enseigne.component;
 
 import enseigne.component.magasin.Magasin;
-import javafx.scene.Node;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -55,4 +49,7 @@ public class ReadConst {
         return magasins;
 
     }
+
+
+
 }
