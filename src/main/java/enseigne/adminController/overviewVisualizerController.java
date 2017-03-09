0927680c4423bc4bdc69bc678661b5854a5487a9
josue.c <jcr.clb@gmail.com>
2017-03-09 22:24:13 +0100

    updated overview controllers

--- src/main/java/enseigne/adminController/overviewVisualizerController.java
+++ src/main/java/enseigne/adminController/overviewVisualizerController.java
@@ -91,7 +91,8 @@ public class overviewVisualizerController {
         XYChart.Series series = new XYChart.Series();
         series.setName(m.getCentre());
         for (int i = 0; i < 5; i++) {
-            String title = Integer.toString((i*2)+8)+"h-" + Integer.toString((i*2)+10) + "h";
+            String title = Integer.toString((i*2)+8)+"h " + Integer.toString((i*2)+10) + "h";
+            System.out.println(title);
             series.getData().add(new XYChart.Data<String, Integer>(title, m.getPointe().get(i)));
         }
         list.add(series);
