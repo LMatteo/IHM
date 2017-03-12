2280907c565dcd69f98d841182a2a28fc52ba459
josue.c <jcr.clb@gmail.com>
2017-03-12 18:01:13 +0100

    added news controller, fxml & images

--- src/main/java/enseigne/customerController/galerie/GallerieController.java
+++ src/main/java/enseigne/customerController/galerie/GallerieController.java
@@ -3,30 +3,97 @@ package enseigne.customerController.galerie;
 import enseigne.ToNode;
 import enseigne.modele.modele.PhotoFilter;
 import enseigne.modele.photo.Photo;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.*;
 
 public class GallerieController {
-    @FXML
-    private VBox pane;
 
-    private PhotoFilter filter;
+    private List<Photo> allPhotos;
+    private Set<String> categories ;
+    String defaultCategory;
 
     @FXML
-    public void initialize() throws IOException{
-        filter = new PhotoFilter();
-        update();
+    private ComboBox<String> categoriesBox;
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private VBox displayPane;
+
+    @FXML
+    public void initialize() throws IOException {
+        allPhotos = getPhotoList();
+        categories = new HashSet<>();
+        categoriesBox.setPromptText("Filtrer");
+        defaultCategory = "Toutes les catégories";
+        categoriesBox.getItems().add(defaultCategory);
+        displayPhotos(allPhotos);
+        displayCategories(allPhotos);
+
     }
 
-    public void update() throws IOException {
-        pane.getChildren().clear();
-        List<Photo> photos = filter.toDisplay();
-        for(Photo photo : photos){
-            pane.getChildren().add(ToNode.photos(photo));
+    public void displayPhotos(List<Photo> list) throws IOException {
+        displayPane.getChildren().clear();
+        for(int i=0;i<list.size();i+=2  ){
+            HBox box = new HBox();
+            box.getChildren().add(ToNode.photos(list.get(i)));
+            if((i+1)<list.size()){
+                box.getChildren().add(ToNode.photos(list.get(i+1)));
+            }
+            displayPane.getChildren().add(box);
         }
     }
+
+    public List<Photo> getPhotoList() throws IOException {
+        return(new PhotoFilter().toDisplay());
+    }
+
+    public void displayCategories(List<Photo> list){
+        for(Photo p : list){
+            categories.add(p.getCategory());
+        }
+        categoriesBox.getItems().addAll(new ArrayList<String>(categories));
+    }
+
+    @FXML
+    void categorySelected(ActionEvent event) throws IOException {
+        List<Photo> photosFiltered = new ArrayList<>();
+        String selected = categoriesBox.getSelectionModel().getSelectedItem();
+        if(Objects.equals(selected, defaultCategory)){
+            photosFiltered = allPhotos;
+        } else {
+            for (Photo p : allPhotos) {
+                if (p.getCategory().equals(selected)) {
+                    photosFiltered.add(p);
+                }
+            }
+        }
+        displayPhotos(photosFiltered);
+    }
+
+
+    @FXML
+    void searchButton(ActionEvent event) throws IOException {
+        String request = searchField.getText();
+        List<Photo> toDisplay = new ArrayList<>();
+        for(Photo p : allPhotos){
+            if(p.getTitreFr().contains(request) ||
+                    p.getTitreEn().contains(request) ||
+                    p.getDescriptionFr().contains(request) ||
+                    p.getDescriptionEn().contains(request)){
+                toDisplay.add(p);
+            }
+        }
+        displayPhotos(toDisplay);
+    }
+
 
 }
--- src/main/java/enseigne/customerController/galerie/PhotoControl.java
+++ src/main/java/enseigne/customerController/galerie/PhotoControl.java
@@ -3,7 +3,10 @@ package enseigne.customerController.galerie;
 import enseigne.modele.photo.Photo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+
+import java.io.File;
 
 public class PhotoControl {
 
@@ -19,6 +22,10 @@ public class PhotoControl {
     public void setPhoto(Photo p){
         title.setText(p.getTitreFr());
         description.setText(p.getDescriptionFr());
+        File f = new File(p.getPhoto());
+        Image i = new Image(f.toURI().toString());
+        image.setImage(i);
     }
+
 
 }
