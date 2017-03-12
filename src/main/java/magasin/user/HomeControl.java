d8ce2e39b8219feadfea72989362979d1f183dfd
Zaki Berouk <zaki.berouk@etu.unice.fr>
2017-03-12 23:44:41 +0100

    fixed minor bug and code improvement

--- src/main/java/magasin/user/HomeControl.java
+++ src/main/java/magasin/user/HomeControl.java
@@ -1,30 +1,17 @@
 package magasin.user;
 
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import sun.plugin.javascript.navig.Anchor;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Observable;
 
 /**
  * @author Zaki
  */
 public class HomeControl {
 
-    @FXML
-    private ScrollPane paneHome;
-    @FXML
-    private AnchorPane apHome;
-
-    private AnchorPane apCatalogue;
-    private ScrollPane paneCatalogue;
-    private ScrollPane paneLayout;
     private LayoutControl layoutControl;
 
     public void setLayoutControl(LayoutControl layoutControl) {
@@ -34,19 +21,16 @@ public class HomeControl {
     @FXML
     void goToCatalogue(MouseEvent event) throws IOException {
         layoutControl.goToCatalogue(null);
-        paneLayout.setFocusTraversable(false);
     }
 
     @FXML
     void goToActu(MouseEvent event) throws IOException {
         layoutControl.goToActu(null);
-        paneLayout.setFocusTraversable(false);
     }
 
     @FXML
     void goToBoutique(MouseEvent event) throws IOException {
         layoutControl.goToBoutique(null);
-        paneLayout.setFocusTraversable(false);
     }
 
 }
