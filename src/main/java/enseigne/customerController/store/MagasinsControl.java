babc959add5940ad27c06c37f6e5be55f1351e3b
Lucas Matteo <lucas.matteo@etu.unice.fr>
2017-03-13 14:04:45 +0100

    la listView marche lo

--- src/main/java/enseigne/customerController/store/MagasinsControl.java
+++ src/main/java/enseigne/customerController/store/MagasinsControl.java
@@ -4,8 +4,10 @@ import enseigne.ToNode;
 import enseigne.adminController.store.MagCell;
 import enseigne.modele.magasin.Magasin;
 import enseigne.modele.modele.MagFilter;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 import java.util.List;
@@ -22,6 +24,6 @@ public class MagasinsControl {
         filter = new MagFilter();
         view.setItems(filter.toDisplay());
         view.setCellFactory((ListView<Magasin> mag ) -> new MagCell());
-
+        view.setFocusTraversable( false );
     }
 }
