459f88a5870b210f66771c2a24deb045bccb2c01
Lucas Matteo <lucas.matteo@etu.unice.fr>
2017-03-14 10:37:20 +0100

    style

--- src/main/java/enseigne/customerController/store/MagasinsControl.java
+++ src/main/java/enseigne/customerController/store/MagasinsControl.java
@@ -4,9 +4,12 @@ import enseigne.ToNode;
 import enseigne.adminController.store.MagCell;
 import enseigne.modele.magasin.Magasin;
 import enseigne.modele.modele.MagFilter;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
@@ -16,7 +19,10 @@ public class MagasinsControl {
 
     @FXML
     ListView<Magasin> view;
-
+    @FXML
+    TextField field;
+    @FXML
+    Button button;
     MagFilter filter;
 
     @FXML
@@ -25,5 +31,11 @@ public class MagasinsControl {
         view.setItems(filter.toDisplay());
         view.setCellFactory((ListView<Magasin> mag ) -> new MagCell());
         view.setFocusTraversable( false );
+    }
+
+    @FXML
+    void filter(ActionEvent event) {
+        filter.setFilter(field.getText());
+        filter.filter();
     }
 }
