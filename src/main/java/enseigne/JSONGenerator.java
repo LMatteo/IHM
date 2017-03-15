ea8226fc304cb2d9c5d306c789085c64a41051b2
josue.c <jcr.clb@gmail.com>
2017-03-15 14:16:34 +0100

    updated jsons

--- src/main/java/enseigne/JSONGenerator.java
+++ src/main/java/enseigne/JSONGenerator.java
@@ -1,7 +1,5 @@
 package enseigne;
 
-import enseigne.modele.ReadConst;
-import enseigne.modele.magasin.Magasin;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -13,15 +11,6 @@ public class JSONGenerator {
 
     @Test
     public void magasin() throws IOException {
-        Magasin m = new Magasin();
-        m.setAddr("13 rue de la paix");
-        m.setPhoto(ReadConst.photoPath+"store1.jpg");
-        m.setCodePostal("06746");
-        m.setVille("Tours");
-        m.setWeb("www.captours-tboth.fr");
-        m.setCentre("CapTours");
-        m.setInfoFr("Nos employés seront fier de vous accueillir du lundi au vendredi de 9h à 18h.");
-        m.setTelephone("0456873554");
-        m.write();
+
     }
 }
